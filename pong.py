677c45d851e2d5c2c58edd3135dcde4dc2f72cdc
sam-rsuarez10 <suarezzcode2001@gmail.com>
2024-04-22 20:07:18 -0400

    add .gitignore and display some empty screen

---
+++ pong.py
@@ -0,0 +1,14 @@
+import pygame
+
+def run_game():
+    ''' Initialize game '''
+
+    pygame.init()
+    pygame.display.set_mode((1500, 750))
+    pygame.display.set_caption("Pong")
+
+    # main loop
+    while True:
+        pass
+
+run_game()
